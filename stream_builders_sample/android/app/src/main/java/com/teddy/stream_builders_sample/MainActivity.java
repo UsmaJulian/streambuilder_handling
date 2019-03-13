a21a88cbd70b0df71ce693bb3c093445143b96a2
Teddy <gedu@gmail.com>
2019-03-13 12:15:41 -0300

    initial commit

---
+++ stream_builders_sample/android/app/src/main/java/com/teddy/stream_builders_sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teddy.stream_builders_sample;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
